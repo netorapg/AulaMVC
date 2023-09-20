1435aa027ccfcd365de99f92f09d661af09548b1
netorapg <renatoplatz11@outlook.com>
2023-09-20 20:52:47 -0300

    new update

--- src/com/coconut/gregory/controll/CoconutController.java
+++ src/com/coconut/gregory/controll/CoconutController.java
@@ -1,5 +1,17 @@
 package com.coconut.gregory.controll;
 
+import com.coconut.gregory.model.diagram.Coconut;
+import com.coconut.gregory.model.service.CoconutService;
+
 public class CoconutController {
+    private CoconutService service;
+    
+    public CoconutController() {
+        service = new CoconutService();
+    }
+
+    public void insert(Coconut coco) {
+        service.insert(coco);
+    }
     
 }
--- src/com/coconut/gregory/model/service/CoconutService.java
+++ src/com/coconut/gregory/model/service/CoconutService.java
@@ -1,5 +1,16 @@
 package com.coconut.gregory.model.service;
 
+import java.util.ArrayList;
+
+import com.coconut.gregory.model.diagram.Coconut;
+
 public class CoconutService {
+    private ArrayList<Coconut> cocos;
+    public void insert(Coconut coco) {
+        if(coco.getWeight() < 500)
+            coco.setGood(false);
+
+        cocos.add(coco);
+    }
     
 }
