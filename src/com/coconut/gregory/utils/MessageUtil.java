31e9badc3adb91649f317a95463a7f1c24c8a653
netorapg <renatoplatz11@outlook.com>
2023-09-20 20:47:34 -0300

    new update

---
+++ src/com/coconut/gregory/utils/MessageUtil.java
@@ -0,0 +1,5 @@
+package com.coconut.gregory.utils;
+
+public class MessageUtil {
+    public static final String INSERT_WEIGHT = "Enter the weight of the coconut: ";
+}
--- src/com/coconut/gregory/view/CoconutView.java
+++ src/com/coconut/gregory/view/CoconutView.java
@@ -2,12 +2,22 @@ package com.coconut.gregory.view;
 
 import java.util.Scanner;
 
+import com.coconut.gregory.controll.CoconutController;
+import com.coconut.gregory.model.diagram.Coconut;
+import com.coconut.gregory.utils.MessageUtil;
+
 public class CoconutView {
     public static void main(String[] args) {
         Scanner tc = new Scanner(System.in);
-
-        System.out.println("Enter the weight of the coconut: ");
+        CoconutController controller = new CoconutController();
+        System.out.println(MessageUtil.INSERT_WEIGHT);
         double weight = tc.nextDouble();
+
+        Coconut coco = new Coconut(true, weight);
+        controller.insert(coco);
+
         
+        
+
     }
 }
